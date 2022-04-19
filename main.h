aded4336a807d81c73fc552b1b423b0440ec1c75
Edwinngera <edwinngera@gmail.com>
2022-04-19 06:40:45 -0700

    modified main

--- main.h
+++ main.h
@@ -1,39 +1,22 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
-
+#ifndef HEADER_HOLBERTON
+#define HEADER_HOLBERTON
 #include <stdarg.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <limits.h>
-#include <stdlib.h>
-
-/**
-* struct types_arguments - Struct op
-*
-* @t: The operator
-* @f: The function associated
-*/
-typedef struct types_arguments
-{
-	char t;
-	int (*f)(va_list va);
-} types;
 int _putchar(char c);
+void _puts(char *str);
 int _strlen(char *s);
-int print_number(va_list va);
-int binary(va_list va);
-int print_unsigned(va_list va);
-int hexa(va_list va);
-int hexa_upper(va_list va);
-int octal(va_list va);
-int print_rot(va_list va);
-char *malloc_str(char *str);
-char *rot13(char *p);
-int print_rev(va_list va);
-int hexa_special(unsigned int c);
-int stringhexa(va_list va);
-int pointer(va_list va);
-int hexa_pointer(unsigned long int c);
 int _printf(const char *format, ...);
-
+char *convert_to(char representation[], unsigned int num, int base);
+int function_manager(char c, va_list arg);
+int _switch(char c, va_list arg);
+int print_character(va_list arg);
+int print_sign(va_list arg, int base);
+int print_unsign(va_list arg, int base);
+int print_string(va_list arg);
+int print_ptr(va_list arg);
+int print_rot13(va_list arg);
+int print_rev(va_list arg);
+int print_base16_upper_lower(va_list arg, char *representation);
+int loop_format(va_list arg, const char *string);
+int call_function_manager(char aux, va_list arg);
+int check_percent(int *flag, char aux);
 #endif
