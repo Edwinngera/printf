4105ebbebb813b1109074272dabc9a458b161a6b
Edwinngera <edwinngera@gmail.com>
2022-04-19 06:04:58 -0700

    make corrections

--- _printf.c
+++ _printf.c
@@ -1,47 +1,132 @@
 #include "main.h"
+
 /**
-* _printf - main function to print in console
-* @format: array to print and check type
-* Return: count of character printed
-**/
-int _printf(const char *format, ...)
+ * t_char - print a character
+ *@va:character
+ *
+ * Return: no return
+ */
+int t_char(va_list va)
 {
-	int count = -1;
+	int c;
 
-	if (format != NULL)
+	c = va_arg(va, int);
+	_putchar(c);
+	return (1);
+}
+/**
+ * t_string - print a string
+ *@va: pointer to string
+ *
+ * Return: no return
+ */
+int t_string(va_list va)
+{
+	int i, j;
+	char n[] = "(null)";
+	char *s = va_arg(va, char *);
+
+	if (s == NULL)
 	{
-		int i;
-		va_list ar_list;
-		int (*o)(va_list);
+		for (i = 0; n[i] != '\0'; i++)
+			_putchar(n[i]);
+		return (6);
+	}
+	for (j = 0; s[j] != '\0'; j++)
+		_putchar(s[j]);
+	return (j);
+}
+/**
+ * print_number - Entry point
+ *@va: the integer to print
+ * Return: no return
+ */
+int print_number(va_list va)
+{
+	int i, len, r, l;
+	unsigned int abs, num, numt;
+	int n = va_arg(va, int);
 
-		va_start(ar_list, format);
-
-		if (format[0] == '%' && format[1] == '\0')
-			return (-1);
-
-		count = 0;
-
-		for (i = 0; format[i] != '\0'; i++)
-		{
-			if (format[i] == '%')
-			{
-				if (format[i + 1] == '%')
-				{
-					count += _putchar(format[i]);
-					i++;
-				}
-				else if (format[i + 1] != '\0')
-				{
-					o = get_func(format[i + 1]);
-					count += (o ? o(ar_list) : _putchar(format[i]) + _putchar(format[i + 1]));
-					i++;
-				}
-			}
-			else
-				count += _putchar(format[i]);
-		}
-		va_end(ar_list);
+	len = 0;
+	i = 0;
+	r = 1;
+	l = 1;
+	if (n < 0)
+	{
+		_putchar('-');
+		len++;
+		abs = -n;
+	} else
+	{
+		abs = n;
 	}
 
-	return (count);
+	num = abs;
+	while (num > 0)
+	{
+		num /= 10;
+		i++;
+	}
+
+	while (r < i)
+	{
+		l *= 10;
+		r++;
+	}
+	while (l >= 1)
+	{
+		numt = (abs / l) % 10;
+		_putchar(numt + '0');
+		len++;
+		l /= 10;
+	}
+	return (len);
+}
+
+/**
+ * _printf - print output according to a format
+ *@format: first argument
+ *
+ * Return: the number of characters printed(excluding the null byte)
+ */
+int _printf(const char *format, ...)
+{
+	int i = 0, j, len = 0, count;
+	va_list valist;
+	types difftypes[] = {{'c', t_char}, {'s', t_string}, {'d', print_number},
+			     {'i', print_number}, {'b', binary}, {'u', print_unsigned},
+			     {'x', hexa}, {'X', hexa_upper}, {'o', octal}, {'R', print_rot},
+			     {'r', print_rev}, {'S', stringhexa}, {'p', pointer}};
+
+	if (format == NULL || (format[0] == '%' && format[1] == 0))
+		return (-1);
+	va_start(valist, format);
+	while (format != NULL && format[i])
+	{
+		if (format[i] != '%')
+			len += _putchar(format[i]);
+		else
+		{
+			i++;
+			if (format[i] == '%')
+				len += _putchar('%');
+			j = 0;
+			count = 0;
+			while (j < 13)
+			{
+				if (format[i] == difftypes[j].t)
+				{
+					len += difftypes[j].f(valist);
+					count = 1;
+					break; }
+				j++; }
+			if (!count && format[i] != '%')
+			{
+				len++;
+				len++;
+				_putchar('%');
+				_putchar(format[i]); }}
+		i++; }
+	va_end(valist);
+	return (len);
 }
